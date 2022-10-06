0e9bff4882aea70ddcc204aa84c96c69129a9138
Olena Korenieva <olena.korenieva@gmail.com>
2022-10-06 14:10:37 +0200

    Update 02-lightbox.js file and README

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const getEl = (selector) => document.querySelector(selector);
+
+const galleryWrapper = getEl(".gallery");
+
+const galleryArr = galleryItems.map(
+  ({
+    original,
+    preview,
+    description,
+  }) => `<a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}">
+     </a>`
+);
+
+galleryWrapper.insertAdjacentHTML("beforeend", galleryArr.join(""));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
